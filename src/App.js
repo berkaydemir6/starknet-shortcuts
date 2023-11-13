f37f7de495632c09a1333fb3fef8ce95d2ec8f2a
Berkay Demir <b3rkaydem1r@yandex.com>
2023-11-14 01:06:47 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import './App.css';
+import logo from './starknet.png'
+import { useState } from 'react';
+
+function App() {
+  const [list, setList] = useState([]);
+
+  function callMenu(menu) {
+    if (menu == 'swap') {
+      setList(['mySwap.xyz', 'Fibrous.finance', 'AVNU.fi', 'Jediswap.xyz', 'Sithswap.com', '10kswap.com', 'Nostra.finance'])
+    }
+    else if (menu == 'nft') {
+      setList(['Flexing.gg', 'Pyramid.market'])
+    }
+    else if(menu == 'other'){
+      setList(['Starknet.id', 'Activity Tracker'])
+    }
+    console.log(menu)
+  }
+
+  const openInNewTab = (url) => {
+    window.open(url, "_blank", "noreferrer");
+  };
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+      </header>
+      <body>
+        <div className='flex-container'>
+          <div className='box box1' onClick={() => callMenu('swap')}>
+            <h2 className='menu'>SWAP</h2>
+          </div>
+          <div className='box box2' onClick={() => callMenu('nft')}>
+            <h2 className='menu'>NFT</h2>
+          </div>
+          <div className='box box3' onClick={() => callMenu('other')}>
+            <h2 className='menu'>OTHER</h2>
+          </div>
+        </div>
+        <div className='flex-container'>
+
+          {
+            list.map(element => {
+              return (
+                <div className='site' onClick={() => openInNewTab('https://' + (element.startsWith('Activity Tracker') ? 'app.rhino.fi/trackers/STARKNET' : element))}>
+                  <h2 className='link'>{element}</h2>
+                </div>)
+            })
+          }
+
+
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
